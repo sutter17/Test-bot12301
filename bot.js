da99b1ca17bf633ea9c7655036e929e77b6c0724
Sutter Phillips <sutterphillips21@gmail.com>
2019-03-28 13:29:55 -0400

    echo

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "hi";
+  botResponse = request.text;
 
   options = {
     hostname: 'api.groupme.com',
